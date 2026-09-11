0907c7b6ca2da0c4f7f727afca81ac81f2807855
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-11 18:45:02 +0100

    feat: bind provider manager and inventory adapter

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Domain\Providers\InventoryNumberProvider;
+use App\Domain\Providers\ProviderManager;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(ProviderManager::class, function ($app) {
+            $manager = new ProviderManager();
+            $manager->register('inventory', $app->make(InventoryNumberProvider::class));
+
+            return $manager;
+        });
     }
 
     /**
