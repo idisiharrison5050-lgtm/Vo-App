dac5fd1e89a649b8a2eab4c1a67a2538d523387e
idisiharrison5050-lgtm <idisiharrison5050@gmail.com>
2026-09-11 16:06:37 +0100

    feat: expose marketplace catalog endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\MarketplaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    Route::get('/marketplace/countries', [MarketplaceController::class, 'countries']);
+    Route::get('/marketplace/services', [MarketplaceController::class, 'services']);
+    Route::get('/marketplace/offers', [MarketplaceController::class, 'offers']);
+
     Route::post('/auth/register', [AuthController::class, 'register'])->middleware('throttle:10,1');
     Route::post('/auth/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
 
